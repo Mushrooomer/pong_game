d772db7c1952ad8a342e380fa91ac2f0d1647dee
Recepce <k.klempir@gmail.com>
2022-09-17 08:53:39 +0200

    Two paddles

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
 
-r_paddle = Paddle((350, 0))
-l_paddle = Paddle((-350, 0))
+
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -10,11 +9,14 @@ screen.bgcolor("black")
 screen.title("PONG")
 screen.tracer(0)
 
-
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
 screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
 
 
 game_is_on = True
